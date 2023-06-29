efa54f0f87afe3025a0e92a1865fddb312f82dda
zhyd8911 <zhyd8911@163.com>
2023-06-29 20:40:48 +0800

    删除demo文件

--- demoProject/firstProject/plus/plus_time_test.go
+++
@@ -1,9 +0,0 @@
-package plus
-
-import "testing"
-
-func TestGetLastMonthLastDsFormat(t *testing.T) {
-	if ans, error := GetLastMonthLastDsFormat(); ans != 202301 && error != nil {
-		t.Errorf("获得上月的数据有问题")
-	}
-}
